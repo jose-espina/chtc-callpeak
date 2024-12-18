3551b4f65579e68db5e7e186384dd3891d75fa44
Jose Espina <Jose@SSITNS-AR1LAB1.local>
2024-12-18 14:17:43 -0600

    Edited sh and sub scripts to use new staging structure and local container

--- callpeak.sh
+++ callpeak.sh
@@ -14,12 +14,20 @@ treatment=$1
 control=$2
 setting=$3
 
+# assign blacklist variable to filename of mm10 blacklist
+blacklist=mm10-blacklist.v2.bed.gz
+chromsizes=mm10.chrom.sizes.txt
+
 # copy $treatment and $control from staging to input directory
-cp /staging/groups/roopra_group/jespina/$treatment input
-cp /staging/groups/roopra_group/jespina/$control input
+cp /staging/jespina/$treatment input
+cp /staging/jespina/$control input
+
+# copy mm10 blacklist to input directory
+cp /staging/jespina/$blacklist input
+cp /staging/jespina/$chromsizes input
 
 # get basename of treatment and assign to $samplename for naming output files
-samplename=`basename $treatment _bowtie2_sorted.bam`
+samplename=`basename $treatment _bowtie2_sorted_mkdup.bam`
 
 # print names of treatment and control files to stdout
 echo "Treatment" $treatment 
@@ -31,17 +39,27 @@ if [ "$setting" == "broad" ]; then
 	-f BAMPE -g mm\
 	-n ${samplename} --broad -B \
 	--outdir output 2> output/${samplename}_macs2.log
+	
+	fileext="broadPeak"
+	
 elif [ "$setting" == "narrow" ]; then
 	macs2 callpeak -t input/$treatment -c input/$control \
 	-f BAMPE -g mm\
 	-n ${samplename} -B \
 	--outdir output 2> output/${samplename}_macs2.log
+
+	fileext="narrowPeak"
 fi
+
+# filter out blacklist regions
+bedtools intersect -a input/${samplename}.${fileext} \
+	-b input/$blacklist -v \
+	> output/${samplename}_blacklist-filtered.${fileext}
 
 # tar macs2 outputs and move to staging
 cd output
 tar -czvf ${samplename}_macs2.tar.gz ./
-mv ${samplename}_macs2.tar.gz /staging/groups/roopra_group/jespina
+mv ${samplename}_macs2.tar.gz /staging/jespina
 cd ~
 
 # before script exits, remove files from working directory
